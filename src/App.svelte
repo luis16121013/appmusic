1cc9211439f09961419550e1c92ef45648b1f076
luispfcanales <lpfunoc@unamad.edu.pe>
2021-06-12 00:29:25 -0500

    app styled

--- src/App.svelte
+++ src/App.svelte
@@ -1,80 +1,21 @@
-<script context="module">
-	export function play({url}){
-		music.src=url
-		music.play()
-	}
-	export function pause(){
-		music.pause()
-	}
-</script>
-
 <script>
-	import StorageMusic from './storage/StorageMusic.js'
 	import CardMusic from './components/CardMusic.svelte'
-	let duration;
-	let muted = false;
-	let currentTime = 0;
-	let paused = false;
-	let volume = 0.55;
-	
-	let nextMusic=0
-		$:{
-				if(duration===currentTime){
-						console.log("termino music")
-						currentTime=0
-						play(StorageMusic[getNextMusic()])
-					}
-			}
-		const reset=()=>{
-				currentTime=0
-			}
-		const view=()=>{
-				console.log(nextMusic)
-				console.log(StorageMusic.length)
-			}
-		const next=()=>{
-				currentTime=0
-			}
-		const getNextMusic=()=>{
-				let allMusicNro=StorageMusic.length - 1 
-				nextMusic+=1
-				if(nextMusic>allMusicNro){
-						nextMusic=0
-						return nextMusic
-					}
-				return nextMusic
-			}
 </script>
 
 <main>
-	<div>
-		<input type="range" min="0" max={duration} step="0.01" value={currentTime} 
-		on:click={e => console.log(e.target.value) }/>
-		<audio id="music"
-				bind:duration={duration}
-				bind:currentTime={currentTime}
-				bind:muted={muted}
-				bind:paused={paused}
-				bind:volume={volume}
-				controls
-				>
-				<track kind="captions">
-		</audio>
-	</div>
-	<button on:click={()=>{play(StorageMusic[nextMusic])}}>play</button>
-	<button on:click={pause}>pause</button>
-	<button on:click={reset}>reset</button>
-	<button on:click={()=>{play(StorageMusic[getNextMusic()])}}>next</button>
-	<button on:click={view}>view</button>
-
 	<CardMusic />
 </main>
 
 <style>
-	audio {
-		visibility: hidden;
-		opacity: 0;
-	}
 
+	main{
+		margin:0;
+		padding:0;
+		display:flex;
+		align-items:center;
+		justify-content:center;
+		height:100vh;
+		background-color:#1e81b0;
+	}
 
 </style>
--- src/components/CardMusic.svelte
+++ src/components/CardMusic.svelte
@@ -1,10 +1,151 @@
-<script>
+<script context="module">
+	export function play({url}){
+		music.src=url
+		music.play()
+	}
+	export function pause(){
+		music.pause()
+	}
+	export function playOn({url}){
+		if(music.src==url){
+			music.play()
+			return
+		}
+		music.src=url
+		music.play()
+	}
 </script>
-<div>
-	<h2>hola</h2>
+
+<script>
+	import StorageMusic from '../storage/StorageMusic.js'
+	let duration;
+	let muted = false;
+	let currentTime = 0;
+	let paused = false;
+	let volume = 0.55;
+
+	let nextMusic=0
+	let buttonPauseView=false
+
+		const viewButton =()=>buttonPauseView=!buttonPauseView
+		const hola=()=>console.log("test")
+
+		$:{
+				if(duration===currentTime){
+						console.log("termino music")
+						currentTime=0
+						play(StorageMusic[getNextMusic()])
+					}
+			}
+		const reset=()=>currentTime=0
+		const next=()=>currentTime=0
+
+		const view=()=>{
+				console.log(nextMusic)
+				console.log(StorageMusic.length)
+			}
+		const getNextMusic=()=>{
+				let allMusicNro=StorageMusic.length - 1 
+				nextMusic+=1
+				if(nextMusic>allMusicNro){
+						nextMusic=0
+						return nextMusic
+					}
+				return nextMusic
+			}
+</script>
+
+<div class="container-card">
+	<div class="button-container">
+		<div class="content-logo">
+			<img class="logo-git"src="./images/github.svg">
+		</div>
+		<input type="range" min="0" max={duration} step="0.01" value={currentTime} />
+		<audio id="music"
+				bind:duration={duration}
+				bind:currentTime={currentTime}
+				bind:muted={muted}
+				bind:paused={paused}
+				bind:volume={volume}
+				controls
+				>
+				<track kind="captions">
+		</audio>
+		<button>
+			<img  src="./images/prev.png" alt="imgs">
+		</button>
+		{#if buttonPauseView}
+			<button on:click={pause}  on:click={viewButton}>
+				<img  class="mid" src="./images/pause.png" alt="img">
+			</button>
+		{:else}
+			<button on:click={()=>{playOn(StorageMusic[nextMusic])}} on:click={viewButton}>
+				<img  class="mid" src="./images/play2.png" alt="img">
+			</button>
+		{/if}
+		<button on:click={()=>{play(StorageMusic[getNextMusic()])}}>
+			<img  class="inv" src="./images/prev.png" alt="img">
+		</button>
+	</div>
 </div>
+
 <style>
-	div{
-		box-shadow: 10px 5px 5px black;
+	.container-card{
+		max-width:450px;
+		background-color:#76b5c5;
+		text-align:center;
+		height:350px;
+		border-radius:10px;
+		border:2px solid #abdbe3;
+		transition:.4s;
+	}
+	.container-card:hover{
+		box-shadow: 0 0 5px 3px  #eee;
+	}
+	.content-logo{
+		width:200px;
+		margin:1.5rem auto;
+	}
+	.logo-git{
+		width:140px;
+	}
+	.logo-git:hover{
+		filter: invert(0);
+	}
+	img {
+		width:50px;
+		background-color:none;
+		filter: invert(0);
+		transition:0.2s;
+	}
+	img:hover {
+		filter: invert(0.3);
+	}
+	.mid {
+		width:55px;
+	}
+	button {
+		margin:0 9px;
+		background-color:#76b5c5;
+		border:none;
+	}
+	button:focus {
+		background-color:#76b5c5;
+	}
+	.button-container {
+		background:none;
+	}
+	.inv {
+		transform:scaleX(-1);
+	}
+
+	audio{
+		position:absolute;
+		visibility:hidden;
+		right:100%;
+		z-index:0;
+	}
+	input{
+		width:100%;
 	}
 </style>
